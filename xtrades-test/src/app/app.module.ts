3c097a4e2f848ee549f446117fa67905ccfc35d1
Yulia Masliak <1985kozak@gmail.com>
2023-07-30 10:06:16 +0300

    feat: add background gradient

--- xtrades-test/src/app/app.module.ts
+++ xtrades-test/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DarkModeComponent } from './dark-mode/dark-mode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkModeComponent
   ],
   imports: [
     BrowserModule,
---
+++ xtrades-test/src/app/dark-mode/dark-mode.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-mode',
+  templateUrl: './dark-mode.component.html',
+  styleUrls: ['./dark-mode.component.css']
+})
+export class DarkModeComponent {}
